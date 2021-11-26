6868ff7d17b45e1dbb436a99d22fda554a3a8655
jessica-ang <82023240+jessica-ang@users.noreply.github.com>
2021-11-26 15:33:41 +0100

    maj sous page professionnel et particulier

--- partenariat.php
+++ partenariat.php
@@ -3,8 +3,19 @@ require("template/header.php");
 ?>
 <section>
     <h3>Entreprises : Partenariat</h3>
-    <div class="div_green">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id diam tempor, fringilla risus vel, venenatis neque. Aenean elit magna, rutrum ac ex sed, viverra scelerisque turpis. Mauris quis eros mauris. Nulla vestibulum ultrices massa id rhoncus. Nulla facilisi. Praesent sodales est sit amet lectus rhoncus pretium. Integer et justo sed sem maximus tempus ac vitae risus. Ut non sapien iaculis, tincidunt tellus et, eleifend elit. Vivamus volutpat malesuada neque, quis cursus lectus semper non. Vestibulum vitae sapien sed velit pharetra cursus nec id leo. Phasellus rutrum, nibh id molestie ornare, ligula nisi aliquet tortor, non lacinia mi neque sit amet justo.</p>
+    <div class="div1_3">
+        <img src="img/">
+        <div>
+            <h6 class="text-uppercase"></h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id diam tempor, fringilla risus vel, venenatis neque. Aenean elit magna, rutrum ac ex sed, viverra scelerisque turpis. Mauris quis eros mauris. Nulla vestibulum ultrices massa id rhoncus. Nulla facilisi. Praesent sodales est sit amet lectus rhoncus pretium. Integer et justo sed sem maximus tempus ac vitae risus. Ut non sapien iaculis, tincidunt tellus et, eleifend elit. Vivamus volutpat malesuada neque, quis cursus lectus semper non. Vestibulum vitae sapien sed velit pharetra cursus nec id leo. Phasellus rutrum, nibh id molestie ornare, ligula nisi aliquet tortor, non lacinia mi neque sit amet justo.</p>
+        </div>
+    </div>
+    <div class="div1_3_pink">
+        <img src="img/">
+        <div>
+            <h6 class="text-uppercase"></h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id diam tempor, fringilla risus vel, venenatis neque. Aenean elit magna, rutrum ac ex sed, viverra scelerisque turpis. Mauris quis eros mauris. Nulla vestibulum ultrices massa id rhoncus. Nulla facilisi. Praesent sodales est sit amet lectus rhoncus pretium. Integer et justo sed sem maximus tempus ac vitae risus. Ut non sapien iaculis, tincidunt tellus et, eleifend elit. Vivamus volutpat malesuada neque, quis cursus lectus semper non. Vestibulum vitae sapien sed velit pharetra cursus nec id leo. Phasellus rutrum, nibh id molestie ornare, ligula nisi aliquet tortor, non lacinia mi neque sit amet justo.</p>
+        </div>
     </div>
 </section>
 
